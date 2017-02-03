cd33ef9bc2cb6154356fca3b3b935e900344b371
Hinanui <benjamin.cornaglia@gmail.com>
2017-02-03 12:42:33 +0100

    [CODE] Update destru + son + health

--- TetrisVR/Assets/Scripts/OBJECTS/Destruction.cs
+++ TetrisVR/Assets/Scripts/OBJECTS/Destruction.cs
@@ -12,6 +12,12 @@ public class Destruction : MonoBehaviour {
     public float m_fDestructionThreshold = 100f;
     public AudioClip m_pClip;
 
+    public GameObject m_pRubble;
+
+    bool m_bHasSpawnedRubble = false;
+    float m_fSpawnRubbleTimer = 0f;
+    public float m_fSpawnTimer = 10f;
+
 	// Use this for initialization
 	void Start () {
         m_pExploder = this.GetComponent<Exploder>();
@@ -19,8 +25,37 @@ public class Destruction : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        ManageSpawnTimer();
+
 	}
+
+    void ManageSpawnTimer()
+    {
+        if (m_bHasSpawnedRubble)
+        {
+            m_fSpawnRubbleTimer += Time.deltaTime;
+        }
+
+        if (m_fSpawnRubbleTimer >= m_fSpawnTimer)
+        {
+            m_fSpawnRubbleTimer = 0;
+            m_bHasSpawnedRubble = false;
+        }
+    }
+
+    void RandomRubbleSpawn(Vector3 _vPos)
+    {
+        int i = Random.Range(0, 100);
+        if(i == 50 || i == 25 || i == 75)
+        {
+            if(!m_bHasSpawnedRubble)
+            {
+                GameObject.Instantiate(m_pRubble, _vPos, new Quaternion(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+                m_bHasSpawnedRubble = true;
+            }
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -41,6 +76,7 @@ public class Destruction : MonoBehaviour {
 
                     }
                 }
+                RandomRubbleSpawn(collision.contacts[0].point);
                 Debug.Log ("Relative Velo = " + collision.relativeVelocity.magnitude * 1000f);
 				Debug.Log ("Explosion Radius = " + Mathf.Clamp(Mathf.Round(fExplosionRadius), 0, m_fMaxExplosionRadius));
             } 
@@ -62,6 +98,7 @@ public class Destruction : MonoBehaviour {
 
                     }
                 }
+                RandomRubbleSpawn(collision.contacts[0].point);
                 Debug.Log("Relative Velo = " + collision.relativeVelocity.magnitude);
                 Debug.Log("Explosion Radius = " + Mathf.Clamp(Mathf.Round(fExplosionRadius), 0, m_fMaxExplosionRadius));
             }
@@ -84,7 +121,7 @@ public class Destruction : MonoBehaviour {
                     }
                 }
                 //Debug.Log(GetComponent<AudioSource>().pitch);
-                
+                RandomRubbleSpawn(collision.contacts[0].point);
                 Debug.Log("Relative Velo = " + collision.relativeVelocity.magnitude);
                 Debug.Log("Explosion Radius = " + Mathf.Clamp(Mathf.Round(fExplosionRadius), 0, m_fMaxExplosionRadius));
             }
