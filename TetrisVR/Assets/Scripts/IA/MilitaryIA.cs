c69f1c96b86fd1574062ae1bed147c8eaf05f52e
green92 <gregoire.chamberland@gmail.com>
2017-02-04 18:37:28 +0100

    Modified military and tank prefabs.

--- TetrisVR/Assets/Scripts/IA/MilitaryIA.cs
+++ TetrisVR/Assets/Scripts/IA/MilitaryIA.cs
@@ -14,21 +14,64 @@ public class MilitaryIA : MonoBehaviour {
 	[Range(5f, 20f)]
 	public float FireInterval = 5f;
 
+	public GameObject WeaponPosition1 = null;
+	public GameObject WeaponPosition2 = null;
+
+	public GameObject Projectile;
+
+	[Range(1, 10)]
+	public int ProjectilePoolSize = 5;
+
+	private bool weaponAlternance;
+
 	private float nextFire;
 
 	private Vector3 lastTargetPosition = Vector3.zero;
 
 	private UnityEngine.AI.NavMeshAgent agent = null;
 
+	private List<GameObject> projectilePool = new List<GameObject>();
+	private int projectileIndex = 0;
+
 	private void fire() {
-		
+		var proj = projectilePool [projectileIndex];
+		if (weaponAlternance) {
+			proj.transform.position = WeaponPosition1.transform.position;
+		} else {
+			if (WeaponPosition2 != null) {
+				proj.transform.position = WeaponPosition2.transform.position;
+			} else {
+				proj.transform.position = WeaponPosition1.transform.position;
+			}
+		}
+		var rb = proj.GetComponent<Rigidbody> ();
+		if (rb != null) {
+			proj.SetActive (true);
+			rb.AddForce ((Target.transform.position - this.transform.position).normalized * 1000.0f);
+		}
+		projectileIndex = (projectileIndex + 1) % ProjectilePoolSize;
+		weaponAlternance = !weaponAlternance;
+	}
+
+	private void createProjectilePool() {
+		for (int i=0; i<ProjectilePoolSize; i++) {
+			var proj = Instantiate (Projectile);
+			proj.SetActive (false);
+			projectilePool.Add (proj);
+		}
 	}
 
 	// Use this for initialization
 	void Start () {
+		if (WeaponPosition1 == null) {
+			throw new UnityException ("MilitaryIA : WeaponPosition1 have to be assigned");
+		}
+
 		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
 
 		nextFire = FireInterval;
+
+		createProjectilePool ();
 	}
 	
 	// Update is called once per frame
