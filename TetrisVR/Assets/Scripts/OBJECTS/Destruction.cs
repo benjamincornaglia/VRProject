2cdad29aef3d45726415cac5819c74b79c73376e
Hinanui <benjamin.cornaglia@gmail.com>
2017-02-20 15:59:09 +0100

    [CODE] Update destruction avancée

--- TetrisVR/Assets/Scripts/OBJECTS/Destruction.cs
+++ TetrisVR/Assets/Scripts/OBJECTS/Destruction.cs
@@ -16,11 +16,19 @@ public class Destruction : MonoBehaviour {
 
     bool m_bHasSpawnedRubble = false;
     float m_fSpawnRubbleTimer = 0f;
-    public float m_fSpawnTimer = 10f;
+    public float m_fSpawnTimer = 2f;
+    public int m_iRubbleStacks = 3;
 
     public bool m_bCanSpawnRubbles = false;
-	// Use this for initialization
-	void Start () {
+
+    List<Vector3> avPos = new List<Vector3>();
+    float fDiff = 0;
+    List<float> afSpeeds = new List<float>();
+
+    public bool m_bGrabbed = false;
+
+    // Use this for initialization
+    void Start () {
         m_pExploder = this.GetComponent<Exploder>();
 	}
 	
@@ -28,7 +36,7 @@ public class Destruction : MonoBehaviour {
 	void Update () {
 
         ManageSpawnTimer();
-
+        CheckMovementVelocity();
 	}
 
     void ManageSpawnTimer()
@@ -53,23 +61,62 @@ public class Destruction : MonoBehaviour {
         {
             if(!m_bHasSpawnedRubble && m_bCanSpawnRubbles)
             {
-                GameObject pRubble = GameObject.Instantiate(m_pRubble, _vPos, new Quaternion(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
-                pRubble.transform.localScale = new Vector3(Random.Range(1, 3), Random.Range(1, 3), Random.Range(1, 3));
-                m_bHasSpawnedRubble = true;
-                pRubble.GetComponent<Destruction>().m_bCanSpawnRubbles = false;
-                pRubble.GetComponent<shaderGlow>().lightOff();
+                //for(int index = 0; index <= m_iRubbleStacks; i++)
+                //{
+                    GameObject pRubble = GameObject.Instantiate(m_pRubble, _vPos, new Quaternion(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+                    pRubble.transform.localScale = new Vector3(Random.Range(1, 3), Random.Range(1, 3), Random.Range(1, 3));
+                    m_bHasSpawnedRubble = true;
+                    pRubble.GetComponent<Destruction>().m_bCanSpawnRubbles = false;
+                    pRubble.GetComponent<shaderGlow>().lightOff();
+                    pRubble.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    pRubble.GetComponent<Rigidbody>().useGravity = true;
+                //}
+                
                 Debug.Log("Rubble Spawn");
             }
         }
+    }
+
+    
+
+    void CheckMovementVelocity()
+    {
+
+        if (avPos.Count > 2)
+        {
+            avPos.RemoveAt(2);
+        }
+
+        avPos.Insert(0, transform.position);
+
+        if(avPos.Count >= 2)
+            fDiff = Vector3.Distance(avPos[0], avPos[1]);
+
+        if (afSpeeds.Count > 10)
+            afSpeeds.RemoveAt(10);
+        afSpeeds.Insert(0, fDiff);
+
+        if (afSpeeds.Count > 3)
+        {
+            if(!m_bGrabbed)
+                fDiff = afSpeeds[2];
+            else
+                fDiff = afSpeeds[2]*10f;
+
+        }
+
+        if (this.gameObject.name == "Cube")
+            Debug.Log("Movement = " + fDiff);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
 		if(this.gameObject.tag == "Destructor" && collision.gameObject.tag == "PicaVoxelVolume")
         {
-            if(collision.relativeVelocity.magnitude * 1000f > m_fDestructionThreshold)
+            if(fDiff > m_fDestructionThreshold)
             {
-                float fExplosionRadius = collision.relativeVelocity.magnitude * 1000f;
+                //float fExplosionRadius = collision.relativeVelocity.magnitude * 1000f;
+                float fExplosionRadius = 1 + fDiff;
                 m_pExploder.ExplosionRadius = Mathf.Clamp(Mathf.Round(fExplosionRadius), 0, m_fMaxExplosionRadius);
                 m_pExploder.Explode();
                 if (GetComponent<AudioSource>() != null)
@@ -89,9 +136,10 @@ public class Destruction : MonoBehaviour {
         }
         else if(this.gameObject.tag == "Piece" && collision.gameObject.tag == "PicaVoxelVolume")
         {
-            if (collision.relativeVelocity.magnitude > m_fDestructionThreshold)
+            if (fDiff > m_fDestructionThreshold)
             {
-                float fExplosionRadius = collision.relativeVelocity.magnitude;
+                float fExplosionRadius = 1 + fDiff;
+                //float fExplosionRadius = Random.Range(2, 10);
                 m_pExploder.ExplosionRadius = Mathf.Clamp(Mathf.Round(fExplosionRadius), 0, m_fMaxExplosionRadius);
                 m_pExploder.Explode();
                 if (GetComponent<AudioSource>() != null)
@@ -113,7 +161,7 @@ public class Destruction : MonoBehaviour {
         {
             if (collision.relativeVelocity.magnitude > m_fDestructionThreshold)
             {
-                float fExplosionRadius = collision.relativeVelocity.magnitude;
+                float fExplosionRadius = 1 + fDiff;
                 m_pExploder.ExplosionRadius = Mathf.Clamp(Mathf.Round(fExplosionRadius), 0, m_fMaxExplosionRadius);
                 m_pExploder.Explode();
                 if (GetComponent<AudioSource>() != null)
